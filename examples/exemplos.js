1c35a98fdf8046d8296e85b55afc8125aed5733e
Lucas Vasconcelos <lucaslg26@yahoo.com.br>
2016-05-14 21:31:14 -0300

    Update

--- examples/exemplos.js
+++ examples/exemplos.js
@@ -11,21 +11,35 @@ Auth.done(function(data) {
   var Cartola = new CartolaAPI({
     glbId: data.glbId
   })
-
-  Cartola.meuTimeInfo(function(data) {
-    console.log("Nome: %s\nCartoleiro: %s", data.time.nome, data.time.nome_cartola)
+  Cartola.meuTimeInfo(function(data, err) {
+    if (err) console.log(err)
+    else
+      console.log("Nome: %s\nCartoleiro: %s", data.time.nome, data.time.nome_cartola)
   })
 
-  Cartola.minhasLigas(function(data) {
-    data.ligas.forEach(function(liga){
-      console.log("Nome: %s\nDescricao: %s", liga.nome, liga.descricao)
-    })
+  Cartola.minhasLigas(function(data, err) {
+    if (err) console.log(err)
+    else
+      data.ligas.forEach(function(liga) {
+        console.log("Nome: %s\nDescricao: %s", liga.nome, liga.descricao)
+      })
   })
 
-  Cartola.clubes(function(data) {
-    Object.keys(data).forEach(function(id){
-      console.log("[%s] Nome: %s\nAbreviação: %s", data[id].id, data[id].nome, data[id].abreviacao);
-    })
+  Cartola.clubes(function(data, err) {
+    if (err) console.log(err)
+    else
+      Object.keys(data).forEach(function(id) {
+        console.log("[%s] Nome: %s\nAbreviação: %s", data[id].id, data[id].nome, data[id].abreviacao);
+      })
+  })
+  Cartola.liga('dengo-kings-ufpa', function(data, err) {
+    if (err) console.log(err)
+    else
+      console.log(data)
   })
 
+})
+
+Auth.catch(function(err) {
+  console.log(err)
 })
